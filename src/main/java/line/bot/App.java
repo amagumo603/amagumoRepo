dc7f69a598541ef6adf61274c4fcbb8739497b1b
amagumo <test@mail.com>
2019-07-02 22:08:00 +0900

    更新

---
+++ src/main/java/line/bot/App.java
@@ -0,0 +1,13 @@
+package line.bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
